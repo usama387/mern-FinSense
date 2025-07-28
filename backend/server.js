fd3ab725357c1b9b8e05d32a86239825b846c5d3
usama387 <usamarazaaq3@gmail.com>
2025-07-28 13:35:12 +0500

    Initial frontend and backend setup completed with tailwindcss, shadcn ui, clerk auth, and prisma orm

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// App Config
+const app = express();
+
+// to access environment variables in .env file
+dotenv.config();
+
+// middlewares
+app.use(express.json());
+
+// Allow ALL origins
+const allowedOrigins = [
+  process.env.PRODUCTION, // Production from env
+  "http://localhost:5173", // Localhost for development
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS: " + origin));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  exposedHeaders: ["set-cookie"],
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
+
+// test api end point
+app.get("/", (req, res) => {
+  res.send("FinSense backend is running successfully...");
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`The backend is up on port ${port}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='font-semibold text-blue-700'>Hello, World</h1>
+    </div>
+  )
+}
+
+export default App
